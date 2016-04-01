fa31a934af22ec4384712ad34150e663fab5d7c4
Neeraj Kumar <kneeraj@snappydata.io>
2016-04-01 20:07:21 +0530

    Code for index check during recovery.

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/store/GemFireStore.java
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/store/GemFireStore.java
@@ -2139,6 +2139,9 @@ public final class GemFireStore implements AccessFactory, ModuleControl,
       // Instead do this at the boot time itself
       CallbackProcedures.clearStatics();
       DistributionObserver.clearStatics();
+      if (!forBoot) {
+        FabricDatabase.TEST_INDEX_RECREATE = false;
+      }
       // remove flag to indicate GemFireXD product
       GemFireCacheImpl.setGFXDSystem(false);
       selfMemId = null;
